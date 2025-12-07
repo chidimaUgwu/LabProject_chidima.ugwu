b9dfc244d554a3d0af3f0f725d5f513635cdb6ec
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-07 12:57:02 +0000

    Update courses.php

--- student/courses.php
+++ student/courses.php
@@ -372,16 +372,16 @@ try {
         <div class="logo-container">
             <img src="../images/logo.png" alt="Company Logo" srcset="">
             <h4 class="dashboard-title">STUDENT DASHBOARD</h4>
-            <div class="user-info">
-                <img src="https://ui-avatars.com/api/?name=<?php echo urlencode($_SESSION['fname'] . '+' . $_SESSION['lname']); ?>&background=4361ee&color=fff" alt="User">
-                <div class="user-details">
-                    <h4><?php echo htmlspecialchars($_SESSION['fname'] . ' ' . $_SESSION['lname']); ?></h4>
-                    <p><?php echo getRoleDisplayName($_SESSION['role']); ?></p>
+            <div class="user-info" style="margin-left: 500px; display: flex; align-items: center; gap: 10px;">
+                <div class="user-avatar"><img src="https://ui-avatars.com/api/?name=<?php echo urlencode($_SESSION['fname'] . '+' . $_SESSION['lname']); ?>&background=4361ee&color=fff" alt="User"></div>
+                <div>
+                    <div class="user-name"><?php echo htmlspecialchars($_SESSION['fname'] . ' ' . $_SESSION['lname']); ?></div>
+                    <div class="user-role"><?php echo getRoleDisplayName($_SESSION['role']); ?></div>
                 </div>
             </div>
         </div>
     </div>
-    
+
     <div class="board">
         <nav>
             <a href="dashboard.php" data-section="dashboard"><i class="fas fa-home"></i> <span>Dashboard</span></a>
