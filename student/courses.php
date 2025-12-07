5dfd983bdf5627b20362d4ba142ac1f5e31e358f
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-07 13:00:16 +0000

    Update courses.php

--- student/courses.php
+++ student/courses.php
@@ -54,6 +54,8 @@ try {
     <title>My Courses - Student Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/Student_Dashboard.css">
+    <link rel="stylesheet" href="../styles/sessionSchedule.css">
     <style>
         .top {
             background-color: #F2EAD3;
