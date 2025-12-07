1853e84f49870bb525732450610f212fe2be1495
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-07 12:46:58 +0000

    Update attendance.php

--- student/attendance.php
+++ student/attendance.php
@@ -87,6 +87,8 @@ try {
     <title>My Attendance - Student Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/Student_Dashboard.css">
+    <link rel="stylesheet" href="../styles/sessionSchedule.css">
     <style>
         /* * {
             margin: 0;
