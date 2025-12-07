0231c84248df0ddefa55a75a1fa24f21775c58cf
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-07 13:01:42 +0000

    Update course_details.php

--- student/course_details.php
+++ student/course_details.php
@@ -96,6 +96,8 @@ try {
     <title><?php echo htmlspecialchars($course['course_code']); ?> - Student Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/Student_Dashboard.css">
+    <link rel="stylesheet" href="../styles/sessionSchedule.css">
     <style>
         * {
             margin: 0;
