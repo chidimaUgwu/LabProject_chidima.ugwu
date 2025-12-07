0446afe361ef27f89a60247d1a66528f4e72cf3a
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-07 13:16:04 +0000

    Update sessions.php

--- faculty/sessions.php
+++ faculty/sessions.php
@@ -201,6 +201,8 @@ try {
     <title>Session Management - Faculty Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/Student_Dashboard.css">
+    <link rel="stylesheet" href="../styles/sessionSchedule.css">
     <style>
         /* Base Layout Styles */
         .top {
