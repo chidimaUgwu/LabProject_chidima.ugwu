7834257b55b98932f6140c3aa389448d4130753e
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-07 13:15:23 +0000

    Update attendance_reports.php

--- faculty/attendance_reports.php
+++ faculty/attendance_reports.php
@@ -94,6 +94,8 @@ try {
     <title>Attendance Reports - Faculty Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/Student_Dashboard.css">
+    <link rel="stylesheet" href="../styles/sessionSchedule.css">
     <style>
         /* Same CSS as dashboard.php - just extract the nav and layout styles */
         .top {
