294b4eb8770bce0a24379025e2ebf3f7b3f379e7
Chidima Praise Jude Ugwu <chidima.ugwu@ashesi.edu.gh>
2025-12-07 13:02:16 +0000

    Update enroll.php

--- student/enroll.php
+++ student/enroll.php
@@ -127,6 +127,8 @@ try {
     <title>Course Enrollment - Student Dashboard</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../styles/Student_Dashboard.css">
+    <link rel="stylesheet" href="../styles/sessionSchedule.css">
     <style>
         .top {
             background-color: #F2EAD3;
